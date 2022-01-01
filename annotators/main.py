73b6bd197ac7f208e688e33cddf76007f17dd012
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-02 02:53:12 +0530

    Update main.py

--- annotators/main.py
+++ annotators/main.py
@@ -3,7 +3,6 @@ from .basic import binary_intent
 
 def annotate(ctx: Context):
     # TODO: add your own annotators
-    ctx = check_language(ctx)
+    ctx = binary_intent(ctx)
     # add annotation in context
-    ctx = previous_label(ctx)
     return ctx
