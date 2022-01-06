986c1de40230fb264d557d40267f56816f1aee41
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-07 02:25:02 +0530

    Update condition.py

--- scenario/condition.py
+++ scenario/condition.py
@@ -1,2 +1,26 @@
-# for conditions
-# TODO: add conditions
+import re
+import logging
+from df_engine.core import Actor, Context
+
+
+
+HEALMED_COMPILED_PATTERN = re.compile(
+    r"(health|Health|medicine|Medicine|mental health|Mental health|diet|Diet|clinical|Clinical|physical examination|^Physical examination.*)",
+    re.IGNORECASE,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+
+def health_medicine_condition(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
+    request = ctx.last_request
+    return bool(HEALMED_COMPILED_PATTERN.search(request))
+
+
+
+def another_health_medicine_condition(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
+    request = ctx.last_request
+    return bool("other" in request or ctx.misc.get("yes_intent"))
+
