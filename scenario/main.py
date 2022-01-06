57de110c86b8ac22c17ea0bf0df77d5ec8302724
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-07 02:25:42 +0530

    Update main.py

--- scenario/main.py
+++ scenario/main.py
@@ -1,13 +1,31 @@
+import df_engine.conditions as cnd
 from df_engine.core import Actor
-from df_engine.core.keywords import RESPONSE
+from df_engine.core.keywords import LOCAL, RESPONSE, TRANSITIONS
 
 
-# TODO: extend graph
+import scenario.condition as loc_cnd
+import scenario.response as rsp
+
+
 plot = {
     "service": {
-        "start": {RESPONSE: ""},
-        "fallback": {RESPONSE: "hi"},
+        LOCAL: {
+            TRANSITIONS: {
+                ("funfact", "random"): loc_cnd.health_medicine_condition,
+            }
+        },
+        "start": {RESPONSE: "hi"},
+        "fallback": {RESPONSE: "Sorry"},
+    },
+    "funfact": {
+        "random": {
+            RESPONSE: rsp.health_medicine_response,
+            TRANSITIONS: {
+                ("funfact", "random"): cnd.any([loc_cnd.health_medicine_condition, loc_cnd.another_health_medicine_condition])
+            },
+        },
     },
 }
+
 
 actor = Actor(plot, start_label=("service", "start"), fallback_label=("service", "fallback"))
