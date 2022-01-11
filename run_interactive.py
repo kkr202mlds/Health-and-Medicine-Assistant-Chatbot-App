0395d7248aa7052ffbda6f4006326c4500e62688
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-11 23:12:30 +0530

    Update run_interactive.py

--- run_interactive.py
+++ run_interactive.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python
-
-import time
+import re
+import logging
 from typing import Optional, Union
 
-from scenario.main import actor
-from df_engine.core import Actor, Context
-from annotators.main import annotate
+from df_engine.core.keywords import TRANSITIONS, RESPONSE
+from df_engine.core import Context, Actor
+import df_engine.conditions as cnd
 
 
 
+# turn_handler - a function is made for the convenience of working with an actor
 def turn_handler(
-    in_request: str,
-    ctx: Union[Context, str, dict],
-    actor: Actor,
-    true_out_response: Optional[str] = None,
+    in_request: str, ctx: Union[Context, str, dict], actor: Actor, true_out_response: Optional[str] = None
 ):
     # Context.cast - gets an object type of [Context, str, dict] returns an object type of Context
     ctx = Context.cast(ctx)
-
     # Add in current context a next request of user
     ctx.add_request(in_request)
-    ctx = annotate(ctx)
-
     # pass the context into actor and it returns updated context with actor response
     ctx = actor(ctx)
     # get last actor response from the context
     out_response = ctx.last_response
     # the next condition branching needs for testing
     if true_out_response is not None and true_out_response != out_response:
-        raise Exception(f"{in_request=} -> true_out_response != out_response: {true_out_response} != {out_response}")
+        msg = f"in_request={in_request} -> true_out_response != out_response: {true_out_response} != {out_response}"
+        raise Exception(msg)
     else:
-        print(f"{in_request=} -> {out_response}")
+        logging.info(f"in_request={in_request} ->\n {out_response}")
     return out_response, ctx
 
 
