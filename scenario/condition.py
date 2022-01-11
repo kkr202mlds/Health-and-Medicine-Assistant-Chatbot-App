a70d1d788e84694b7fc4af197d7e768136f89728
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-11 07:39:20 +0530

    Update and Modify Condition.py

--- scenario/condition.py
+++ scenario/condition.py
@@ -1,26 +1,31 @@
-import re
-import logging
-from df_engine.core import Actor, Context
+from typing import Optional, Union, Any
+from df_engine.core import Context, Actor
+from data.appointment_    import appointment
+from data.doctor_         import doctor
+from data.health_         import health
+from data.medicine_       import medicine
+from data.diet_           import diet
+from data.mental_health_  import mental_health
 
 
-
-HEALMED_COMPILED_PATTERN = re.compile(
-    r"(health|Health|medicine|Medicine|mental health|Mental health|diet|Diet|clinical|Clinical|physical examination|^Physical examination.*)",
-    re.IGNORECASE,
-)
-
-
-logger = logging.getLogger(__name__)
-
-
-
-def health_medicine_condition(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
+def healmed_request(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
     request = ctx.last_request
-    return bool(HEALMED_COMPILED_PATTERN.search(request))
-
-
-
-def another_health_medicine_condition(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
-    request = ctx.last_request
-    return bool("other" in request or ctx.misc.get("yes_intent"))
-
+    request = request.lower()
+    if re.search(r'\bappointment\b|\bdoctor', request, re.IGNORECASE):
+        if re.search(r'\bappointment\b|\bdoctor', request, re.IGNORECASE)[0] == 'appointment':
+            return appointment(request)
+        else:
+            return doctor(request)
+    elif re.search(r'\bhealth\b|\bmedicine', request, re.IGNORECASE):
+        if re.search(r'\bhealth\b|\bmedicine', request, re.IGNORECASE)[0] == 'health':
+            return health(request)
+        else:
+            return medicine(request)
+    elif re.search(r'\bdiet\b|\bmental_health', request, re.IGNORECASE):
+        if re.search(r'\bdiet\b|\bmental_health', request, re.IGNORECASE)[0] == 'diet':
+            return diet(request)
+        else:
+            return mental_health(request)
+    elif re.search(r'\b|Physicians|Cardiologists|Gastroenterologists|Dentist|ENT specialist|Gynaecologist', request, re.IGNORECASE):
+        return  """OK! I have book a appointment of {} Doctor for you Sir""".format(request)
+    return "Sure Sir, I thanks for your visit that but your can go other services if needed you can type 'hi'"
