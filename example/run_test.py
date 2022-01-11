bc0f985fe2412d0acd3f80a492121944c4d4dad3
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-11 21:34:46 +0530

    Update run_test.py

--- example/run_test.py
+++ example/run_test.py
@@ -1,19 +1,20 @@
 import random
-
 from scenario.main import actor
 import run_interactive
 
 random.seed(314)
 
+
 # testing
 testing_dialog = [
     ("hi", "Sorry"),
     (
-        "funfact",
-        "Some sea snakes can breathe through their skin. Would you like to hear another fun fact?",
+        "health",
+        "Health refers to the extent of a person’s physical, mental, and social well-being. This definition, taken from the World Health Organization’s treatment of health, emphasizes that health is a complex concept that involves not just the soundness of a person’s body but also the state of a person’s mind and the quality of the social environment in which she or he lives. The quality of the social environment in turn can affect a person’s physical and mental health, underscoring the importance of social factors for these twin aspects of our overall well-being.",
     ),
-    ("yes", "A 2007 study found that music, classical in particular, can help make plants grow faster. Would you like to hear another fun fact?"),
+    ("yes", "Mental health as a state of well-being in which the individual realizes his or her own abilities, can cope with the normal stresses of life, can work productively and fruitfully, and is able to make a contribution to his or her community", "diet": "Diet includes a variety of plant-based and animal-based foods that provide nutrients to the body. Such nutrients provide the body with energy and keep it running. Nutrients help build and strengthen bones, muscles, and tendons and also regulate body processes (i.e., blood pressure). Water is essential for growth, reproduction and good health. Macronutrients are consumed in relatively large quantities and include proteins, carbohydrates, and fats and fatty acids. Micronutrients – vitamins and minerals – are consumed in relatively smaller quantities, but are essential to body processes"),
 ]
+
 
 
 def run_test():
@@ -23,5 +24,6 @@ def run_test():
     print("test passed")
 
 
+    
 if __name__ == "__main__":
     run_test()
