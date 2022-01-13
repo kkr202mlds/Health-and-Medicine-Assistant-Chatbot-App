28879d9764c992dadd1571bc147e8001ea5e0915
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-13 22:59:57 +0530

    Create Creating_X_train_Y_train.py

---
+++ model/Implementation/Creating_X_train_Y_train.py
@@ -0,0 +1,14 @@
+form Data_Preprocessing import xy
+from nltk_utils import bag_of_words
+
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
