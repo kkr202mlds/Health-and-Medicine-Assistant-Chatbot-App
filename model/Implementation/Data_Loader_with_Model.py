1f0f66f1abe93dd4af76b4de21eb289e8f6a0abf
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-14 00:16:48 +0530

    Update Data_Loader_with_Model.py

--- model/Implementation/Data_Loader_with_Model.py
+++ model/Implementation/Data_Loader_with_Model.py
@@ -2,6 +2,12 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from Chat_Dataset  import ChatDataset
+from Hyper-parameters import batch_size, input_size, hidden_size, output_size
+from Neural_Network_Model import NeuralNet
+from Creating_X_train_Y_train import X_train, y_train 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 dataset = ChatDataset()
