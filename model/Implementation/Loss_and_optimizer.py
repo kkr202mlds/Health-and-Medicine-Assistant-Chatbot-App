e3fa86cb7332afe1c7aafe77526cc8bec66cd760
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-14 00:23:06 +0530

    Update Loss_and_optimizer.py

--- model/Implementation/Loss_and_optimizer.py
+++ model/Implementation/Loss_and_optimizer.py
@@ -1,7 +1,9 @@
 #PyTorch
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from Data_Loader_with_Model import model
+from Hyper-parameters import learning_rate
+
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
