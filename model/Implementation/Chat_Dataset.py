4e64ef99ec857e770f9e9ba90bd1ea12ff71fb11
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-13 21:24:23 +0530

    Create Chat_Dataset.py

---
+++ model/Implementation/Chat_Dataset.py
@@ -0,0 +1,18 @@
+#PyTorch
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+
+class ChatDataset(Dataset):
+
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
