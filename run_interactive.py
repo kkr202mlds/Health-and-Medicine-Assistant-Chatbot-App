2df5c95721e6c7e42d1a976d3fe5e6acf656039b
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-01-28 03:23:30 +0530

    Add files via upload

--- run_interactive.py
+++ run_interactive.py
@@ -2,13 +2,11 @@ import time
 import re
 import logging
 from typing import Optional, Union
-
 from df_engine.core.keywords import TRANSITIONS, RESPONSE
 from df_engine.core import Context, Actor
 import df_engine.conditions as cnd
 from scenario.main import actor
-from  scenario.condition import healmed_request
-
+from scenario.response import run_interactive_mode
 
  
 
@@ -31,24 +29,3 @@ def turn_handler(
     else:
         logging.info(f"in_request={in_request} ->\n {out_response}")
     return out_response, ctx
-
-
-
-def run_interactive_mode(actor):
-    ctx = {}
-    while True:
-        in_request = input("type your answer: ")
-        st_time = time.time()
-        _, ctx = turn_handler(in_request, ctx, actor)
-        total_time = time.time() - st_time
-        print(f"exec time = {total_time:.3f}s")
-
-
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s-%(name)15s:%(lineno)3s:%(funcName)20s():%(levelname)s - %(message)s", level=logging.INFO
-    )
-    
-#     run_test()
-    run_interactive_mode(actor)
