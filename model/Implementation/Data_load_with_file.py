287e9d84c6f55d49b47dc42f0a40bf314086f7a9
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-02-05 18:20:10 +0530

    Delete model directory

--- model/Implementation/Data_load_with_file.py
+++
@@ -1,5 +0,0 @@
-import json
-
-
-with open('data/health_medicine.json', 'r') as f:
-    intents = json.load(f)
--- model/Implementation/Loss_and_optimizer.py
+++
@@ -1,9 +0,0 @@
-#PyTorch
-import torch
-import torch.nn as nn
-from Data_Loader_with_Model import model
-from Hyper-parameters import learning_rate
-
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
