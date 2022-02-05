5e03c8596b89d1a42c94e0e1960161639d26939e
Kkumar-20 <96828026+Kkumar-20@users.noreply.github.com>
2022-02-05 18:50:46 +0530

    Add files via upload

---
+++ data/Danish/doctor_.py
@@ -0,0 +1,2 @@
+def doctor(user_input):
+    return"Der er mange læger som følgende:\n- Læger\n- Kardiologer\n- Gastroenterologer\n- Tandlæge\n- ØNH-specialist\n- Gynækolog\nSkriv venligst typen af læge i henhold til ovenstående måde"
---
+++ data/Danish/mental_health_.py
@@ -0,0 +1,2 @@
+def mental_health(user_input):
+    return "Mental sundhed refererer til kognitivt, adfærdsmæssigt og følelsesmæssigt velvære. Det handler om, hvordan folk tænker, føler og opfører sig. Folk bruger nogle gange udtrykket 'mental sundhed' til at betyde fraværet af en psykisk lidelse. Mental sundhed kan påvirke dagligdagen, relationer og fysisk sundhed.Psykisk sundhed som en tilstand af velvære, hvor individet realiserer sine egne evner, kan klare livets normale belastninger, kan arbejde produktivt og frugtbart, og er i stand til at yde et bidrag til sit samfund."
